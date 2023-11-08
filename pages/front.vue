5563922df5a7a1eb916514eabc603ce8a32f734a
Nikhil Rai <nikhilrai662@gmail.com>
2023-11-08 16:42:49 +0530

    first commit

--- pages/front.vue
+++ pages/front.vue
@@ -1,94 +1,107 @@
 <template>
-<div class="page-container">
-    <v-container fluid>
-        <v-row>
-            <v-col cols="12">
+    <div class="page-container">
+        <v-container fluid>
+            <v-row>
+                <v-col cols="12">
 
-                <v-card class="mx-auto">
-                    <v-layout>
-                        <v-app-bar style="background-color: #4a61c1;" image="https://picsum.photos/1920/1080?random">
-                            <template v-slot:image>
-                                <v-img gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"></v-img>
-                            </template>
+                    <v-card class="mx-auto">
+                        <v-layout>
+                            <v-app-bar style="background-color: #4a61c1;" image="https://picsum.photos/1920/1080?random">
+                                <template v-slot:image>
+                                    <v-img gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"></v-img>
+                                </template>
 
-                            <template v-slot:prepend>
-                                <v-app-bar-nav-icon></v-app-bar-nav-icon>
-                            </template>
+                                <template v-slot:prepend>
+                                    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+                                </template>
 
-                            <v-app-bar-title>
+                                <v-app-bar-title>
+                                    <v-btn icon>
+                                        <v-icon class="iconicon">mdi-menu</v-icon>
+                                    </v-btn>
+                                </v-app-bar-title>
+
+                                <v-spacer></v-spacer>
+
                                 <v-btn icon>
-                                    <v-icon class="iconicon">mdi-menu</v-icon>
+                                    <v-icon class="iconicon">mdi-bell</v-icon>
                                 </v-btn>
-                            </v-app-bar-title>
 
-                            <v-spacer></v-spacer>
+                                <v-btn icon>
+                                    <v-icon class="iconicon">mdi-share-variant</v-icon>
+                                </v-btn>
 
-                            <v-btn icon>
-                                <v-icon class="iconicon">mdi-bell</v-icon>
-                            </v-btn>
+                                <v-btn icon>
+                                    <v-icon class="iconicon">mdi-magnify</v-icon>
+                                </v-btn>
+                            </v-app-bar>
+                        </v-layout>
+                    </v-card>
 
-                            <v-btn icon>
-                                <v-icon class="iconicon">mdi-share-variant</v-icon>
-                            </v-btn>
+                </v-col>
+            </v-row>
 
-                            <v-btn icon>
-                                <v-icon class="iconicon">mdi-magnify</v-icon>
-                            </v-btn>
-                        </v-app-bar>
+            <v-row style="background-image: https://www.shutterstock.com/image-vector/programming-code-coding-hacker-background-260nw-1714491562.jpg;">
+                <v-col cols="12" class="mt-5">
+                    <div style="display:flex; align-items:center; justify-content:center ">
+                        <h2 class="textcolor">AUTOMATE YOUR TASKS IN A SMARTER WAY</h2>
+                    </div>
+                    <div>
+                        <p style="color:black !important">{{ apiResponse }}</p>
 
-                        <!-- <v-main>
-                              <v-container fluid>
-                                <v-row dense>
-                                  <v-col
-                                    v-for="n in 4"
-                                    :key="n"
-                                    cols="12"
-                                  >
-                                    <v-card
-                                      :title="`Content ${n}`"
-                                      :subtitle="`Subtitle for Content ${n}`"
-                                      text="Lorem ipsum dolor sit amet consectetur, adipisicing elit.?"
-                                    ></v-card>
-                                  </v-col>
-                                </v-row>
-                              </v-container>
-                            </v-main> -->
-                    </v-layout>
-                </v-card>
+                    </div>
+                </v-col>
+                <v-col cols="12" style="display: flex; justify-content: center; align-items: center">
+                    <template>
+                        <router-link to="/">
+                            <v-btn style="background-color: rgb(196, 193, 193);  padding: 20px; ">Continue to Prompting
+                            <!-- <v-icon id="arrow"
+                                style="color: white; font-size: 30px;background-color: #4a61c1;border-radius: 50px; cursor: pointer;">
+                                mdi-play
+                            </v-icon> -->
+                        </v-btn>
+                        </router-link>
+                        
+                    </template>
+                </v-col>
+            </v-row>
 
-            </v-col>
-        </v-row>
-
-        <v-row>
-            <v-col cols="12" class="mt-5">
-                <div style="display:flex; align-items:center; justify-content:center ">
-                    <h2 class="textcolor">AUTOMATE YOUR TASKS IN A SMARTER WAY</h2>
-                </div>
-            </v-col>
-        </v-row>
-
-        <v-row>
-            <v-col cols="12" class="mt-2">
-              <div style="display:flex; align-items:center; justify-content:center">
-                <img
-                  :src="require('~/assets/images/artificial-intelligence.png')"
-                  :alt="imageAltText"
-                  
-                >
-              </div>
-            </v-col>
-        </v-row>
-    </v-container>
-</div>
+            <v-row>
+                <v-col cols="12" class="mt-2">
+                    <div style="display:flex; align-items:center; justify-content:center">
+                        <img :src="require('~/assets/images/artificial-intelligence.png')" :alt="imageAltText">
+                    </div>
+                </v-col>
+            </v-row>
+            
+        </v-container>
+    </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     data() {
         return {
             imageSource: '@/assets/images/artificial-intelligence.png', // Image ka path specify karein
             imageAltText: 'Alternative Text for Image', // Image ke liye alt text
+            apiResponse: '',
         };
+
+    },
+    methods: {
+        fetchData() {
+            axios.get('http://127.0.0.1:8082/api/') // Replace with your actual endpoint
+                .then((response) => {
+                    this.apiResponse = response.data[0].answer; // Adjust based on your response structure
+                })
+                .catch((error) => {
+                    console.error('Error fetching data:', error);
+                });
+        },
+    },
+    created() {
+        this.fetchData();
     },
 };
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,206 +1,232 @@
 <template>
-<v-container fluid style="background-color:white !important">
-    <v-row>
-        <v-col cols="12" class=" textcenter">
-            <v-card style="background-color:#ebf0e6;">
-                <v-row no-gutters>
-                    <v-col cols="2">
-                        <v-icon class="save-icon">mdi-content-save-outline</v-icon>
-                    </v-col>
-                    <v-col cols="10">
-                    </v-col>
-                    <v-col cols="12">
-                        <v-card-text>
-                            <h1 class="custom-heading" >KM PRIYANKA</h1>
-                        </v-card-text>
-                    </v-col>
-                </v-row>
-            </v-card>
-        </v-col>
-    </v-row>
-
-    <v-row>
-        <v-col cols="12">
-            <h4 class="define" style="color:black !important">
-                Define Your Automation Flow
-            </h4>
-        </v-col>
-    </v-row>
-
-    <v-row>
-
-        <v-col cols="12" style="display:flex; justify-content:center; align-items:center;">
-            <v-card style="background-color:#ebf0e6;">
-
-                <v-card-text>
-                    <h4 style="color:#4a61c1;">Task: Former Suite</h4>
-                </v-card-text>
-
-            </v-card>
-        </v-col>
-
-    </v-row>
-
-    <v-container>
+    <v-container fluid style="background-color:white !important">
         <v-row>
-            <v-col cols="12" sm="3">
-            </v-col>
-            <v-col cols="12" sm="6" class=" mt-5">
-                <v-card>
-                    <v-row>
-                        <v-col cols="12" style="background-color:#4a61c1;" class="d-flex justify-end align-center">
-                            <v-icon class="view-icon icon-with-gap eyeicon1" >mdi-eye</v-icon>
-                            <v-icon class="search-icon icon-with-gap eyeicon1">mdi-magnify</v-icon>
-                            <v-icon @click="openDialog" size="x-large" class="icon-spacing eyeicon1">mdi-pencil</v-icon>
-                            <v-dialog v-model="dialogVisible" max-width="500">
-                                <v-card style="background-color: #4a61c1">
-                                    <v-card-title class="eyeicon1">Dialog Title</v-card-title>
-                                    <v-card-text>
-                                        <v-responsive class="mx-auto" max-width="344">
-                                            <v-text-field class="mt-5 " label="Type Here..." hide-details="auto" outlined></v-text-field>
-                                        </v-responsive>
-                                    </v-card-text>
-                                    <v-card-actions>
-                                        <v-btn @click="dialogVisible = false" style="background-color:rgb(0, 5, 32)" class="eyeicon1">Close</v-btn>
-                                    </v-card-actions>
-                                </v-card>
-                            </v-dialog>
-
+            <v-col cols="12" class=" textcenter">
+                <v-card style="background-color:#ebf0e6;">
+                    <v-row no-gutters>
+                        <v-col cols="2">
+                            <v-icon class="save-icon">mdi-content-save-outline</v-icon>
                         </v-col>
-
-                    </v-row>
-
-                    <v-row>
-                        <v-col cols="12" style="background-color: #4a61c1">
-                            <div class="icon-and-text-container">
-                                <v-avatar>
-                                    <v-icon style="font-size: 48px;" class="eyeicon1">mdi-weather-partly-cloudy</v-icon>
-                                </v-avatar>
-                                <p style="color:white !important">Lorem ipsum dolor sit amet consectetur adipisicing eli olor sit amet consectetur adipisicing elit. Magnam consectetur adipisicing elit. Magnam, quam?</p>
-                            </div>
+                        <v-col cols="10">
+                        </v-col>
+                        <v-col cols="12">
+                            <v-card-text>
+                                <h1 class="custom-heading">AutoSuit</h1>
+                            </v-card-text>
                         </v-col>
                     </v-row>
                 </v-card>
             </v-col>
-            <v-col cols="12" sm="3">
-            </v-col>
-            <v-col cols="12" style="display: flex; justify-content:center; align-items:center">
-                <div>
-                    <v-icon style=" font-size:30px" >
-                        mdi-chevron-down
-                    </v-icon>
-
-                </div>
+        </v-row>
+        <v-row>
+            <v-col cols="12">
+                <h4 class="define" style="color:black !important">
+                    Define Your Automation Flow
+                </h4>
             </v-col>
         </v-row>
-    </v-container>
-
-    <!-- <v-row style="margin-bottom:290px">
-        <v-col cols="12" style="display: flex; justify-content:center; align-items:center">
-            <div>
-                <v-icon style="color:#4a61c1; font-size:60px">
-                    mdi-chevron-down
-                </v-icon>
-
-            </div>
-        </v-col>
-    </v-row> -->
-    <v-container>
-        <v-row v-for="(box, index) in newBoxes" :key="index" style="margin-top: 5px;">
-            <v-col cols="12" sm="3"></v-col>
-            <v-col cols="12" sm="6" style="background-color: #4a61c1">
-                <v-card>
-                    <v-row>
-                        <v-col cols="12" style="background-color:#4a61c1" class="d-flex justify-end align-center">
-                            <v-icon class="view-icon icon-with-gap eyeicon1">mdi-eye</v-icon>
-                            <v-icon class="search-icon icon-with-gap eyeicon1">mdi-magnify</v-icon>
-                            <v-icon @click="openDialog" size="x-large" class="icon-spacing eyeicon1">mdi-pencil</v-icon>
-                            <v-dialog v-model="dialogVisible" max-width="500">
-                                <v-card style="background-color: #26c6da">
-                                    <v-card-title>Dialog Title</v-card-title>
-                                    <v-card-text>
-                                        <v-responsive class="mx-auto" max-width="344">
-                                            <v-text-field class="mt-5" label="Type Here..." hide-details="auto" outlined></v-text-field>
-                                        </v-responsive>
-                                    </v-card-text>
-                                    <v-card-actions>
-                                        <v-btn @click="dialogVisible = false" style="background-color:rgb(0, 5, 32)">Close</v-btn>
-                                    </v-card-actions>
-                                </v-card>
-                            </v-dialog>
-    
-                        </v-col>
-    
-                    </v-row>
-    
-                    <v-row>
-                        <v-col cols="12" style="background-color: #4a61c1">
-                            <div class="icon-and-text-container">
-                                <v-avatar>
-                                    <v-icon style="font-size: 48px;" class="eyeicon1">mdi-weather-partly-cloudy</v-icon>
-                                </v-avatar>
-                                <p class="eyeicon1">Lorem ipsum dolor sit amet consectetur adipisicing eli olor sit amet consectetur adipisicing elit. Magnam consectetur adipisicing elit. Magnam, quam?</p>
-                            </div>
-                        </v-col>
-                    </v-row>
+        <v-row>
+            <v-col cols="12" style="display:flex; justify-content:center; align-items:center;">
+                <v-card style="background-color:#ebf0e6;">
+                    <v-card-text>
+                        <h4 style="color:#4a61c1;">Task: Former Suite</h4>
+                    </v-card-text>
                 </v-card>
             </v-col>
-            <v-col cols="12" sm="6"></v-col>
-    
-            <v-col cols="12" style="display: flex; justify-content:center; align-items:center">
-                <div>
-                    <v-icon style="color:#4a61c1; font-size:30px">
-                        mdi-chevron-down
-                    </v-icon>
-    
-                </div>
-            </v-col>
-    
         </v-row>
-
-
-        <v-row>
-            <v-col cols="12" style="display: flex; justify-content: center; align-items: center">
-                <v-icon id="arrow" style="color: #4a61c1; font-size: 40px; cursor: pointer" @click="addArrow">
-                    mdi-chevron-down
-                </v-icon>
-    
-            </v-col>
-            <v-col cols="12" style="display: flex; justify-content: center; align-items: center">
-                <template>
-                    <v-btn style="background-color: rgb(196, 193, 193);  padding: 20px; ">
-                        <v-icon id="arrow" style="color: white; font-size: 30px;background-color: #4a61c1;border-radius: 50px; cursor: pointer;">
-                            mdi-play
+        <v-container>
+            <v-row>
+                <v-col cols="12" sm="3">
+                </v-col>
+                <v-col cols="12" sm="6" class=" mt-5">
+                    <v-card>
+                        <v-row>
+                            <v-col cols="12" style="background-color:#4a61c1;" class="d-flex justify-end align-center">
+                                <v-icon class="view-icon icon-with-gap eyeicon1">mdi-eye</v-icon>
+                                <v-icon class="search-icon icon-with-gap eyeicon1">mdi-magnify</v-icon>
+                                <v-icon @click="openDialog" size="x-large" class="icon-spacing eyeicon1">mdi-pencil</v-icon>
+                                <v-dialog v-model="dialogVisible" max-width="500">
+                                    <v-card style="background-color: #4a61c1">
+                                        <v-card-title class="eyeicon1">Expert Prompt</v-card-title>
+                                        <v-card-text>
+                                            <v-responsive class="mx-auto" max-width="344">
+                                                <v-text-field class="mt-5 " v-model="dialogQuestion" label="Type Here..."
+                                                    hide-details="auto" outlined></v-text-field>
+                                            </v-responsive>
+                                        </v-card-text>
+                                        <v-card-actions>
+                                            <v-btn @click="dialogVisible = false" style="background-color:rgb(0, 5, 32)"
+                                                class="eyeicon1">Close</v-btn>
+                                            <v-btn @click="submitData1">Button</v-btn>
+                                        </v-card-actions>
+                                    </v-card>
+                                </v-dialog>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="12" style="background-color: #4a61c1">
+                                <div class="icon-and-text-container">
+                                    <v-avatar>
+                                        <v-icon style="font-size: 48px;" class="eyeicon1">mdi-weather-partly-cloudy</v-icon>
+                                    </v-avatar>
+                                    <v-text-field v-model="question" label="Enter Your First Prompt..."
+                                        hide-details="auto" outlined></v-text-field>
+                                    <v-btn @click="submitData" style=" margin-left: 5px;">Button</v-btn>
+                                </div>
+                            </v-col>
+                        </v-row>
+                    </v-card>
+                </v-col>
+                <v-col cols="12" sm="3">
+                </v-col>
+                <v-col cols="12" style="display: flex; justify-content:center; align-items:center">
+                    <div>
+                        <v-icon style=" font-size:30px">
+                            mdi-chevron-down
                         </v-icon>
-                    </v-btn>
-    
-                </template>
-            </v-col>
-        </v-row>
+                    </div>
+                </v-col>
+            </v-row>
+        </v-container>
+        <v-container>
+            <v-row v-for="(box, index) in newBoxes" :key="index" style="margin-top: 5px;">
+                <v-col cols="12" sm="3"></v-col>
+                <v-col cols="12" sm="6" style="background-color: #4a61c1">
+                    <v-card>
+                        <v-row>
+                            <v-col cols="12" style="background-color:#4a61c1" class="d-flex justify-end align-center">
+                                <v-icon class="view-icon icon-with-gap eyeicon1">mdi-eye</v-icon>
+                                <v-icon class="search-icon icon-with-gap eyeicon1">mdi-magnify</v-icon>
+                                <v-icon @click="openDialog" size="x-large" class="icon-spacing eyeicon1">mdi-pencil</v-icon>
+                                <v-dialog v-model="dialogVisible" max-width="500">
+                                    <v-card style="background-color: #26c6da">
+                                        <v-card-title>Expert Prompt</v-card-title>
+                                        <v-card-text>
+                                            <v-responsive class="mx-auto" max-width="344">
+                                                <v-text-field class="mt-5" v-model="dialogQuestion" label="Type Here..."
+                                                    hide-details="auto" outlined></v-text-field>
+                                            </v-responsive>
+                                        </v-card-text>
+                                        <v-card-actions>
+                                            <v-btn @click="dialogVisible = false" class="eyeicon1"
+                                                style="background-color:rgb(0, 5, 32)">Close</v-btn>
+                                            <v-btn @click="submitData1">Button</v-btn>
+                                        </v-card-actions>
+                                    </v-card>
+                                </v-dialog>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="12" style="background-color: #4a61c1">
+                                <div class="icon-and-text-container">
+                                    <v-avatar>
+                                        <v-icon style="font-size: 48px;" class="eyeicon1">mdi-weather-partly-cloudy</v-icon>
+                                    </v-avatar>
+                                    <v-text-field v-model="question" label="Enter Your First Prompt..."
+                                        hide-details="auto" outlined></v-text-field>
+                                    <v-btn @click="submitData" style="margin-left: 5px;">Button</v-btn>
+                                </div>
+                            </v-col>
+                        </v-row>
+                    </v-card>
+                </v-col>
+                <v-col cols="12" sm="6"></v-col>
+                <v-col cols="12" style="display: flex; justify-content:center; align-items:center">
+                    <div>
+                        <v-icon style="color:#4a61c1; font-size:30px">
+                            mdi-chevron-down
+                        </v-icon>
+                    </div>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col cols="12" style="display: flex; justify-content: center; align-items: center">
+                    <v-icon id="arrow" style="color: #4a61c1; font-size: 40px; cursor: pointer" @click="addArrow">
+                        mdi-chevron-down
+                    </v-icon>
+                </v-col>
+                <v-col cols="12" style="display: flex; justify-content: center; align-items: center">
+                    <template>
+                        <router-link to="/front">
+                            <v-btn style="background-color: rgb(196, 193, 193);  padding: 20px; ">
+                            <v-icon id="arrow"
+                                style="color: white; font-size: 30px;background-color: #4a61c1;border-radius: 50px; cursor: pointer;">
+                                mdi-play
+                            </v-icon>
+                        </v-btn>
+                        </router-link>
+                        
+                    </template>
+                </v-col>
+            </v-row>
+        </v-container>
     </v-container>
-   
-    
-   
-
-</v-container>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     data() {
         return {
             dialogVisible: false,
             newBoxes: [],
+            question: '',
+            dialogQuestion: '',
+            apiResponse: null,
         };
     },
     methods: {
         openDialog() {
             this.dialogVisible = true;
+            const text = this.question;
+            const parts = text.split(':');
+            if (parts.length > 1) {
+                this.dialogQuestion = parts[1].trim();
+            } else {
+                this.dialogQuestion = '';
+            }
         },
         addArrow() {
-            // Push a new box to the newBoxes array on arrow click
             this.newBoxes.push({});
         },
+        fetchData() {
+            axios.get('http://127.0.0.1:8082/api/')
+                .then((response) => {
+                    console.log('API response:', response.data);
+                    this.apiResponse = response.data;
+                })
+                .catch((error) => {
+                    console.error('Error fetching data:', error);
+                });
+        },
+        submitData() {
+            console.log('question:', this.question);
+
+            axios
+                .post('http://127.0.0.1:8082/api/', { question: this.question })
+                .then((response) => {
+                    console.log('API response:', response.data);
+                })
+                .catch((error) => {
+                    console.error('Error submitting data:', error);
+                });
+        },
+        submitData1() {
+            console.log('dialogQuestion:', this.dialogQuestion);
+
+            axios
+                .post('http://127.0.0.1:8082/api/', { question: this.dialogQuestion })
+                .then((response) => {
+                    this.apiResponse = response.data.answer;
+                    console.log('API response:', response.data);
+                })
+                .catch((error) => {
+                    console.error('Error submitting data:', error);
+                });
+        },
+        created() {
+            this.fetchData();
+        }
     },
 };
 </script>
@@ -246,14 +272,11 @@ export default {
 
 .icon-and-text-container {
     display: flex;
-    /* Use flexbox */
     align-items: center;
-    /* Vertically center items */
 }
 
 .icon-and-text-container p {
     margin-left: 10px;
-    /* Add some space between the icon and text */
 }
 
 .icon-with-gap {
@@ -261,7 +284,7 @@ export default {
 }
 
 
-.eyeicon1{
-    color:white ;
+.eyeicon1 {
+    color: white;
 }
 </style>
